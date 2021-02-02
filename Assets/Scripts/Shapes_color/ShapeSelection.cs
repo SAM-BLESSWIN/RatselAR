df043bfc951a88c07906b2c4464c50be588fb5d5
sam lucifer <samblesswinedu@gmail.com>
2021-02-02 23:53:56 +0530

    Lean Touch SDK

---
+++ Assets/Scripts/Shapes_color/ShapeSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ShapeSelection : MonoBehaviour
+{
+    public GameObject square;
+
+    public void sq()
+    {
+        Instantiate(square,transform.position,Quaternion.identity);
+    }
+    public void rect()
+    {
+        Toast.Instance.Show("rectangle");
+    }
+    public void tri()
+    {
+        Toast.Instance.Show("triangle");
+    }
+    public void circle()
+    {
+        Toast.Instance.Show("circle");
+    }
+}
