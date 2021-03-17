199933cacbb53b127a261f74a3bfa59e19ea45b3
sam lucifer <samblesswinedu@gmail.com>
2021-03-17 18:35:44 +0530

    Levelsadded

---
+++ Assets/Scripts/ImageTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTester : MonoBehaviour
+{
+    private ARTrackedImageManager aRTrackedImageManager;
+
+    private void Awake()
+    {
+        aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+
+}
