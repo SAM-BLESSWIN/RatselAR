3c1f2f6bb0f7369897764d58e658c7d8d30d3220
sam lucifer <samblesswinedu@gmail.com>
2021-03-25 01:45:17 +0530

    Login Done

--- Assets/Scripts/Playfab/PlayfabManager.cs
+++ Assets/Scripts/Playfab/PlayfabManager.cs
@@ -8,43 +8,126 @@ using TMPro;
 
 public class PlayfabManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject rowprefab;
-    [SerializeField]
-    private Transform rowparent;
+    private string emailinput;
+    private string pwdinput;
+    private string conpwdinput;
+    private string nameinput;
+
     [SerializeField]
     private TMP_Text messagetext;
     [SerializeField]
-    private InputField usernameinput;
+    private GameObject login;
     [SerializeField]
-    private InputField emailinput;
+    private GameObject register;
     [SerializeField]
-    private InputField pwdinput;
+    private GameObject reset;
     [SerializeField]
-    private GameObject namewindow;
+    private GameObject leaderbdicon;
     [SerializeField]
-    private GameObject leaderboardwindow;
+    private GameObject logicon;
+    [SerializeField]
+    private GameObject gameicon;
+    [SerializeField]
+    private GameObject logouticon;
 
     private void Start()
     {
-        login();
-    }
-
-    private void login()
-    {
-        var request = new LoginWithCustomIDRequest
+        if (PlayerPrefs.HasKey("EMAIL"))
         {
-            CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true,
-            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
-            {
-                GetPlayerProfile = true
-            }
-        };
-        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, Onerror);
+            Login();
+        }
     }
 
-    public void Sendlevel(int level)
+    public void Getusername(string usernameIn)
+    {
+        nameinput = usernameIn;
+    }
+    public void GetUsermailid(string usermailidIn)
+    {
+        emailinput = usermailidIn;
+    }
+    public void Getuserpassword(string passwordIn)
+    {
+        pwdinput = passwordIn;
+    }
+    public void Getuserconpassword(string passwordIn)
+    {
+        conpwdinput = passwordIn;
+    }
+
+    private void Login()
+    {
+        emailinput = PlayerPrefs.GetString("EMAIL");
+        pwdinput = PlayerPrefs.GetString("PASSWORD");
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailinput,
+            Password = pwdinput,
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, Onerror);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        messagetext.text = "Logged in";
+        Debug.Log("Congratulations, you made your first successful API call!");
+        // int l = PlayerPrefs.GetInt("levelsunlocked");
+        // Sendlevel(l + 1);
+        login.SetActive(false);
+        logicon.SetActive(false);
+        gameicon.SetActive(true);
+        logouticon.SetActive(true);
+        leaderbdicon.SetActive(true);
+    }
+
+    public void Register()
+    {
+        messagetext.text = " ";
+        if (pwdinput.Length < 6)
+        {
+            messagetext.text = "Password too short";
+        }
+
+        if(pwdinput==conpwdinput)
+        {
+            var request = new RegisterPlayFabUserRequest
+            {
+                Username = nameinput,
+                Email = emailinput,
+                Password = pwdinput,
+                RequireBothUsernameAndEmail = true
+            };
+            PlayFabClientAPI.RegisterPlayFabUser(request, Onregistersucccess, Onerror);
+        }
+        else
+        {
+            messagetext.text = "Pasword mismatch";
+        }
+        
+    }
+
+    private void Onregistersucccess(RegisterPlayFabUserResult result)
+    {
+        messagetext.text = "Registered and logged in!";
+        PlayerPrefs.SetString("EMAIL", emailinput);
+        PlayerPrefs.SetString("PASSWORD", pwdinput);
+        register.SetActive(false);
+    }
+
+    public void Onclicklogin()
+    {
+        messagetext.text = " ";
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailinput,
+            Password = pwdinput,
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, Onerror);
+    }
+
+    
+
+    /*public void Sendlevel(int level)
     {
         var request = new UpdatePlayerStatisticsRequest
         {
@@ -64,6 +147,8 @@ public class PlayfabManager : MonoBehaviour
     {
         Debug.Log("User statistics updated");
     }
+
+   
 
     public void Getleaderboardstats()
     {
@@ -89,100 +174,43 @@ public class PlayfabManager : MonoBehaviour
             TMP_Text[] texts = newgo.GetComponentsInChildren<TMP_Text>();
             int pos = item.Position + 1;
             texts[0].text = pos.ToString();
-            texts[1].text = item.PlayFabId;
+            texts[1].text = item.DisplayName;
             texts[2].text = item.StatValue.ToString();
 
-            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+            Debug.Log(item.Position + " " + item.DisplayName + " " + item.StatValue);
         }
     }
+*/
+    
 
-    public void Register()
-    {
-        if(pwdinput.text.Length <6)
-        {
-            messagetext.text = "Password too short";
-        }
-
-        var request = new RegisterPlayFabUserRequest
-        {
-            Email = emailinput.text,
-            Password = pwdinput.text,
-            RequireBothUsernameAndEmail = false
-        };
-        PlayFabClientAPI.RegisterPlayFabUser(request, Onregistersucccess, Onerror);
-    }
-
-    public void Login()
-    {
-        var request = new LoginWithEmailAddressRequest
-        {
-            Email = emailinput.text,
-            Password = pwdinput.text
-        };
-        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, Onerror);
-    }
+    
 
     public void Resetpwd()
     {
+        messagetext.text = " ";
         var request = new SendAccountRecoveryEmailRequest
         {
-            Email=emailinput.text,
+            Email=emailinput,
             TitleId= "823BE",
         };
         PlayFabClientAPI.SendAccountRecoveryEmail(request,Onpwdreset,Onerror);
     }
 
-    private void Onregistersucccess(RegisterPlayFabUserResult result)
-    {
-        messagetext.text = "Registered and logged in!";
-    }
-
-    private void OnLoginSuccess(LoginResult result)
-    {
-       // messagetext.text = "Logged in";
-        Debug.Log("Congratulations, you made your first successful API call!");
-        int l = PlayerPrefs.GetInt("levelsunlocked");
-        Sendlevel(l + 1);
-
-        string name = null;
-        if(result.InfoResultPayload.PlayerProfile !=null)
-        {
-            name = result.InfoResultPayload.PlayerProfile.DisplayName;
-        }
-
-        if(name==null)
-        {
-            namewindow.SetActive(true);
-        }
-        else
-        {
-            leaderboardwindow.SetActive(true);
-        }
-    }
-
     private void Onpwdreset(SendAccountRecoveryEmailResult result)
     {
         messagetext.text = "Password reset mail sent";
-    }
-
-    public void Submitname()
-    {
-        var request = new UpdateUserTitleDisplayNameRequest
-        {
-            DisplayName = usernameinput.text
-        };
-        PlayFabClientAPI.UpdateUserTitleDisplayName(request, Onnameset, Onerror);
-    }
-
-    private void Onnameset(UpdateUserTitleDisplayNameResult result)
-    {
-        Debug.Log("updated display name");
-        leaderboardwindow.SetActive(true);
+        reset.SetActive(false);
     }
 
     private void Onerror(PlayFabError error)
     {
         messagetext.text = error.ErrorMessage;
         Debug.LogError(error.GenerateErrorReport());
+    }
+
+    public void logout()
+    {
+        PlayerPrefs.DeleteKey("EMAIL");
+        PlayerPrefs.DeleteKey("PASSWORD");
     }
 }
---
+++ Assets/Scripts/Sceneloading/MenuCanvasManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCanvasManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject log;
+    [SerializeField]
+    private GameObject lboard;
+    [SerializeField]
+    private GameObject reg;
+    [SerializeField]
+    private GameObject reset;
+
+    public void login()
+    {
+        log.SetActive(true);
+    }
+    public void loginclose()
+    {
+        log.SetActive(false);
+    }
+    public void leaderbd()
+    {
+        lboard.SetActive(true);
+    }
+    public void leaderbdclose()
+    {
+        lboard.SetActive(false);
+    }
+    public void regis()
+    {
+        reg.SetActive(true);
+    }
+    public void regisclose()
+    {
+        reg.SetActive(false);
+    }
+    public void resetpwd()
+    {
+        reset.SetActive(true);
+    }
+    public void resetpwdclose()
+    {
+        reset.SetActive(false);
+    }
+}
