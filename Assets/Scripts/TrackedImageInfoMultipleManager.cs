bc53ab776e0012848dff963f663ea373f4436810
sam lucifer <samblesswinedu@gmail.com>
2021-03-27 12:49:27 +0530

    ImageTracking Done

--- Assets/Scripts/TrackedImageInfoMultipleManager.cs
+++ Assets/Scripts/TrackedImageInfoMultipleManager.cs
@@ -6,14 +6,21 @@ using UnityEngine.XR.ARFoundation;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class AlphabetChecker : MonoBehaviour
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImageInfoMultipleManager : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text alphabets;
     [SerializeField]
+    private TMP_Text debugcount;
+    [SerializeField]
+    private TMP_Text debugimg;
+    [SerializeField]
     private GameObject crct;
     [SerializeField]
     private GameObject wrong;
+    [SerializeField]
+    private GameObject change;
     [SerializeField]
     private Transform spawnpoint;
     [SerializeField]
@@ -37,14 +44,15 @@ public class AlphabetChecker : MonoBehaviour
         count = 0;
         curralpha = alpha[count];
         alphabets.text = curralpha;
+        debugcount.text = count.ToString();
         count++;
     }
 
     void Awake()
     {
         m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
-
-        foreach (GameObject arObject in arObjectsToPlace)
+        
+        foreach(GameObject arObject in arObjectsToPlace)
         {
             GameObject newARObject = Instantiate(arObject, spawnpoint.position, Quaternion.identity);
             newARObject.name = arObject.name;
@@ -59,14 +67,14 @@ public class AlphabetChecker : MonoBehaviour
 
     void OnDisable()
     {
-        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+          m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
     }
 
     void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
-            UpdateARImage(trackedImage);
+             UpdateARImage(trackedImage);
         }
         foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
@@ -74,35 +82,36 @@ public class AlphabetChecker : MonoBehaviour
         }
         foreach (ARTrackedImage trackedImage in eventArgs.removed)
         {
-            arObjects[trackedImage.name].SetActive(false);
+           arObjects[trackedImage.name].SetActive(false);
         }
     }
 
     private void UpdateARobject(ARTrackedImage trackedImage)
     {
-        if (trackedImage.referenceImage.name == curralpha)
-        {
-            if (arObjectsToPlace != null)
+            if (trackedImage.referenceImage.name == curralpha)
             {
-                goARObject = arObjects[trackedImage.referenceImage.name];
-                goARObject.SetActive(true);
-                goARObject.transform.position = trackedImage.transform.position;
-                foreach (GameObject go in arObjects.Values)
+                if (arObjectsToPlace != null)
                 {
-                    Debug.Log($"Go in arObjects.Values: {go.name}");
-                    if (go.name != trackedImage.referenceImage.name)
+                    goARObject = arObjects[trackedImage.referenceImage.name];
+                    goARObject.SetActive(true);
+                    goARObject.transform.position = trackedImage.transform.position;
+                    foreach (GameObject go in arObjects.Values)
                     {
-                        go.SetActive(false);
+                        Debug.Log($"Go in arObjects.Values: {go.name}");
+                        if (go.name != trackedImage.referenceImage.name)
+                        {
+                            go.SetActive(false);
+                        }
                     }
                 }
             }
-        }
 
     }
 
     private void UpdateARImage(ARTrackedImage trackedImage)
     {
-        AssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position);
+       debugimg.text = trackedImage.referenceImage.name;
+       AssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position);
     }
 
     void AssignGameObject(string name, Vector3 newPosition)
@@ -118,8 +127,8 @@ public class AlphabetChecker : MonoBehaviour
         }
         else
         {
-            wrong.SetActive(true);
-            StartCoroutine(wtimer());
+             wrong.SetActive(true);
+             StartCoroutine(wtimer());
         }
     }
 
@@ -133,11 +142,15 @@ public class AlphabetChecker : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
 
-        if (run)
+        if(run)
         {
             Destroy(goARObject);
             crct.SetActive(false);
             arObjects.Remove(curralpha);
+          //  change.SetActive(true);
+
+            debugcount.text = count.ToString();
+
             curralpha = alpha[count];
 
             if (count < alpha.Length)
@@ -149,6 +162,14 @@ public class AlphabetChecker : MonoBehaviour
             {
                 //gameover
             }
+          //  StartCoroutine(changer());
         }
     }
+
+   /* IEnumerator changer()
+    {
+        yield return new WaitForSeconds(5f);
+        run = false;
+        change.SetActive(false);
+    }*/
 }
